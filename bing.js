e820ff69b86c7a5ea736b03556f62f2325af4727
Sergey Lezhnin <54681029+ExZuperi@users.noreply.github.com>
2024-05-05 22:30:09 +0500

    Initial commit

---
+++ bing.js
@@ -0,0 +1,23 @@
+let linksDictionary = {};
+
+function parseAndLogLinks() {
+  const linkElements = document.querySelectorAll('a[class="tilk"]');
+  linkElements.forEach(linkElement => {
+    const link = linkElement.getAttribute('href');
+    if (!(link in linksDictionary)) {
+      linksDictionary[link] = true;
+      console.log(link);
+    }
+  });
+
+  browser.storage.local.set({ links: Object.keys(linksDictionary) }, function() {
+    console.log('Links saved to storage');
+  });
+}
+
+const observer = new MutationObserver(parseAndLogLinks);
+observer.observe(document.body, { childList: true, subtree: true });
+
+window.addEventListener('scroll', () => {
+  browser.runtime.sendMessage({ action: 'updateLinksCount' });
+});
